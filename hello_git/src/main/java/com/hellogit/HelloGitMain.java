0e427842fa315f5b8d801ebb0d48620754c40224
p81238109 <39752409+p81238109@users.noreply.github.com>
2020-12-02 15:35:08 +0800

    修改HelloGitMain类，试验git pull功能

--- hello_git/src/main/java/com/hellogit/HelloGitMain.java
+++ hello_git/src/main/java/com/hellogit/HelloGitMain.java
@@ -3,5 +3,7 @@ package com.hellogit;
 public class HelloGitMain {
     public static void main(String[] args) {
         System.out.println("第一个IDEA+Git的项目,hello_git!");
+        
+        System.out.println("在Github上手动修改一下HelloGitMain类，试验git pull功能!");
     }
 }
